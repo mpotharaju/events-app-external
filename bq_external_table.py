9ac57f027679d1c042306d9b73c082da54fc2ee8
mpotharaju <mukta.potharaju@gmail.com>
2021-11-15 15:47:11 -0600

    Update bq_external_table.py

--- bq_external_table.py
+++ bq_external_table.py
@@ -1,88 +1,150 @@
 ###########################################################################################
 # Arguments:                                                                              
-#   trigger file name
-###########################################################################################
-import sys
-import uuid
-from google.cloud import bigquery
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-print('Argument 1:', str(sys.argv[1]))  # 1st argument
-
-# Construct a BigQuery client object.
-client = bigquery.Client(project="ford-gcp-ingest-d")
-print("Client creating using default project: {}".format(client.project))
-
-# Construct the source URI from the 1st argument
-# (Sample trigger file format: table=NCVD035_PJ1_VIN_LIST&cvdc62_partition_month_x=2021-04.trg)
+#   1. trigger file name
+#   2. bq_project_name
+#   3. historical load config file
+#   4. historical load dataset name
+# Logic:
+# (Sample trigger file format: 
+#   table=ncvdc62_fnv2_ev_usa_sec_hte;cvdc62_partition_month_x=2021-04.trg)
 # 1st Argument format: <bucketname>/<other-path>/tablename-partitionname
+# Construct the source URI from the 1st argument
 # 1. Fetch the tablename and partitionname
 # 2. Get the configurations for the table from HistoricalLoad_config.json
 # 3. Load the .json and get the value for the datafiles root path
 # 4. Add the current partition name to the root path
 # 5. Create temporary external table for the partition datafiles
 # 6. Insert the data from interim to final using the query in config file
+###########################################################################################
+import sys
+import uuid
+import fnmatch
+from google.cloud import bigquery
+from google.cloud import storage  
 
-bqProject = 'ford-gcp-ingest-d'
-datasetName = 'Test'
-tableName = 'HistoricalLoad_config'
-configFileUri = 'https://storage.cloud.google.com/mukta-historical-load/config/Historical_Load_config.json'
-configFileTable = 'Test.HistoricalLoad_config'
+def moveFile(bucket_from, bucket_to, files_path):
+    storage_client = storage.Client()
+    source_bucket = storage_client.bucket(bucket_from)
+    target_bucket = storage_client.bucket(bucket_to)
+    #print('source_bucket: ', source_bucket)
+    blobs = source_bucket.list_blobs(prefix = files_path, delimiter = '/') 
 
-################################################
+    for blob in blobs:
+        #print('blob is: ', blob.name)
+        #print('files_path: ', files_path)
+        if (blob.name != files_path): 
+            source_bucket.copy_blob(blob, target_bucket, blob.name)
+            source_bucket.delete_blob(blob.name)
+            print('Moved blob ', blob.name, ' to bucket ', target_bucket, ', Path ', files_path)
+
+print('Number of arguments: ', len(sys.argv), 'arguments.')
+print('Argument List: ', str(sys.argv))
+
+# TODO: Error if argument count mis-match
+triggerFileName = sys.argv[1]
+bqProject = sys.argv[2]
+historicalLoadConfigFileUri = sys.argv[3]
+datasetName = sys.argv[4]
+
+print('Trigger file name: ', triggerFileName)
+print('BQ ProjectName: ', bqProject)
+print('HistoricalLoad ConfigFile: ', historicalLoadConfigFileUri)
+print('historicalLoad Dataset: ', datasetName)
+
+# Construct a BigQuery client object.
+client = bigquery.Client(project=bqProject)
+print("Client created using project: {}".format(client.project))
+
+##################################################
 # Read the config file and load it into a BQ table
-################################################
-# TODO: Get the jobname from the pubsub event name
-loadToRun = 'ncvdc62_fnv2_ev_usa_sec_hte_ext'
+##################################################
+
+# Generate a unique name for the configFileTable
 jobUuid = str(uuid.uuid4().int)
-configFileTable = 'Test.HistoricalLoad_config'+'_'+jobUuid
+configFileTableNamePart = 'HistoricalLoad_config_'+jobUuid
+configFileTable = datasetName+'.'+configFileTableNamePart
 print('Config file table name: ',configFileTable)
 
-#tableId = TableId.of(BQproject,datasetName, tableName);
+# Create the load job config and load the table into BQ
 loadconfig = bigquery.LoadJobConfig(
                 source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
                 autodetect=True)
-config_job = client.load_table_from_uri(configFileUri, configFileTable,
+config_job = client.load_table_from_uri(historicalLoadConfigFileUri, configFileTable,
                     job_config=loadconfig)  
 print('Starting job {}'.format(config_job.job_id))    
 config_job.result()                
-# Retrieve the config table
-configFileTableId = client.get_table(configFileTable)
 
-################################################
+#######################################################
 # Query the config table for the current run properties
-################################################
+#######################################################
+triggerFileName = triggerFileName.replace('.trg','')
+tableInfoStr = triggerFileName.split(';')
+print('tableStr: ',tableInfoStr)
+tableToLoad = tableInfoStr[0]
+partitionFolder = tableInfoStr[1]
+print('partitionFolder: ',partitionFolder)
 query = 'SELECT * FROM '+configFileTable+' where JobID = @job_id and Active = @activeFlag ';
-print('Query is ',query);
+#print('Query is ',query);
 
 query_config = bigquery.QueryJobConfig(query_parameters=[
-                    bigquery.ScalarQueryParameter('job_id', 'STRING', loadToRun),
+                    bigquery.ScalarQueryParameter('job_id', 'STRING', tableToLoad),
                     bigquery.ScalarQueryParameter('activeFlag', 'BOOL', 'true')
                 ], use_legacy_sql=False)
 queryResult = client.query(query, job_config=query_config)
 print('The query data: ')
 for row in queryResult:
    print('Config is: ',row)
-   print('Data file path: ',row.DataFilePath)
-   print('Data file archive path: ',row.DataFileArchivePath)
-   print('Insert query: ',row.InsertQuery)
-#print('Insert Query is: ',insertQuery)
+   dataSet = row.DataSet
+   dataFilePath = row.DataFilePath
+   insertQuery = row.InsertQuery
+   sourceFormat = row.SourceFormat
+   sourceBucket = row.SourceBucket
+   processedBucket = row.ProcessedBucket
+   errorBucket = row.ErrorBucket
+   archiveFiles = row.ArchiveFiles
+   active = row.Active
+
+# Fetch the location of the data files (excluding the source bucket name)
+# Eg: dataFilePath = 'gs://<bucket-name>/Input/ncvdc62_fnv2_ev_usa_sec_hte_ext_bad/'
+#     relativeFilesPath should be set to Input/ncvdc62_fnv2_ev_usa_sec_hte_ext_bad/
+pathArray = dataFilePath.replace('gs://', '').split('/')
+relativeFilesPath = '/'.join(pathArray[1:])
+print('relativeFilesPath: ', relativeFilesPath)
 
 # Configure the external data source and query job.
-uri = 'gs://mukta-poc-sqlserver/Input/ncvdc62_fnv2_ev_usa_sec_hte_ext/cvdc62_partition_month_x=2021-04/000008_0'
-external_config = bigquery.ExternalConfig("ORC")
-external_config.source_uris = [
-    uri
-]
-table_id = "TestOrcExt"
-job_config = bigquery.QueryJobConfig(table_definitions={table_id: external_config})
+try:
+    uri = dataFilePath+partitionFolder+'/*'
+    print('URI is: ',uri)
+    external_config = bigquery.ExternalConfig(sourceFormat)
+    external_config.source_uris = [
+        uri
+    ]
+    table_id = "TestOrcExt"
+    job_config = bigquery.QueryJobConfig(table_definitions={table_id: external_config})
 
-# Example query to find states starting with 'W'.
-#sql = 'SELECT * FROM `{}` WHERE name LIKE "W%"'.format(table_id)
-sql = 'insert into Test.tmp_final (cvdc62_sha_k) select cvdc62_sha_k FROM `{}` '.format(table_id)
+    # Replace the source table name and the target table name
+    insertQuery = insertQuery.replace('EXTERNAL_TABLE', table_id)
+    insertQuery = insertQuery.replace('TARGET_TABLE', dataSet+'.'+tableToLoad)
+    #print('Insert Query is: ',insertQuery)
 
-query_job = client.query(sql, job_config=job_config)  # Make an API request.
+    query_job = client.query(insertQuery, job_config=job_config)  # Make an API request.
 
-w_states = list(query_job)  # Wait for the job to complete.
-print("There are {} rows inserted into final".format(len(w_states)))
+    w_states = list(query_job)  # Wait for the job to complete.
+    print('Success: Data inserted into ', tableToLoad)
+    if (archiveFiles):
+        print('Moving the files to processed bucket: ', processedBucket, ', Path: ', relativeFilesPath)
+        moveFile(sourceBucket, processedBucket, relativeFilesPath)
+except Exception as e:
+    print('Error: ', e)
+    if (archiveFiles):
+        print('Moving the files to error bucket: ', errorBucket, ', Path: ', relativeFilesPath)
+        moveFile(sourceBucket, errorBucket, relativeFilesPath)
+
+
+### Delete the temp config table
+client.delete_table(configFileTable, not_found_ok=True)
+
+
+
+
+
